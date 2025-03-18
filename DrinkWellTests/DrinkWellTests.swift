0927f2551d6cea389e4dbea1dd121fb3b9e6022e
hilalfnisanci <hilalfnisanci@gmail.com>
2025-03-18 22:03:37 +0300

    Initial Commit

---
+++ DrinkWellTests/DrinkWellTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkWellTests.swift
+//  DrinkWellTests
+//
+//  Created by Hilal on 18.03.2025.
+//
+
+import Testing
+@testable import DrinkWell
+
+struct DrinkWellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
